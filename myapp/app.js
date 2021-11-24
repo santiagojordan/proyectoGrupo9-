07f9ac2dcd4dc54f2fce682c16bc223e7acdc734
santiagojordan <sjordan@udesa.edu.ar>
2021-11-24 08:22:36 -0300

    login

--- myapp/app.js
+++ myapp/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/registracion', registracionRouter);
-app.use('./login', loginRouter)
+app.use('/login', loginRouter)
 
 
 // catch 404 and forward to error handler
