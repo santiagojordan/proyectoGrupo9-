54040f26c741d1ab18311d8d116d1b6d48366ed0
santiagojordan <sjordan@udesa.edu.ar>
2021-11-24 00:58:15 -0300

    Cambios varios en vista index

--- myapp/controllers/indexController.js
+++ myapp/controllers/indexController.js
@@ -1,18 +1,23 @@
 const db = require('../database/models');
+const usuarios = require('../database/models/usuarios');
+const posts = require('../data/posteos')
 const Op = db.Sequelize.Op;
 
 const indexController = {
     index: (req, res) => {
         db.Posteo.findAll({
-            include: [{
-                all:true,
+            include: [
+                //all:true,
                 //nested: true
-            }],
+                {association: 'Usuario'},
+                {association: 'comentarios', include: 'usuarios'}
+            ],
            
             }).then(posts => {
                 /*res.send(postsViejos)*/
                 res.render('index',{
-                    posts: posts
+                    posts: posts,
+
                 })
             
             })
